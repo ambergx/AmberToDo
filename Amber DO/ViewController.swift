585db19443161ac1ec1a31a2b57c366cead4cc85
Amber Gu <ambergu@192.168.0.108>
2020-08-30 16:50:34 +0800

    display to-do list with mock data

---
+++ Amber DO/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  Amber DO
+//
+//  Created by Amber Gu on 2020/8/30.
+//  Copyright © 2020 Amber Gu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var tableView: UITableView!
+    
+    var tasks = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.title = "To Do List"
+        
+        tasks.append("Amber")
+        tasks.append("Jun")
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+}
+
+extension ViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+}
+
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = tasks[indexPath.row]
+    
+        return cell
+    }
+}
+
